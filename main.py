ef4cb108a63f7f0eb89a407187cbfa8ee398f706
David Trower <david@trowertech.com>
2024-07-16 13:41:05 -0500

    Create random_color() function to generate a random RGB color

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from codex import *
+
+# Initialize global variables
+bg_color = (0, 0, 0)
+
+# Generate a randomly generated RGB color
+def random_color():
+    red = random.randrange(0, 256)
+    green = random.randrange(0, 256)
+    blue = random.randrange(0, 256)
+    return red, green, blue # returns a tuple in (r, g, b) format
